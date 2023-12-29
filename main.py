11141380f5efca673d745881cd02d17b68be8b67
rox-streuli <roxstreuli@gmail.com>
2023-12-29 13:27:41 +0000

    Create starter worm

--- main.py
+++ main.py
@@ -5,4 +5,22 @@ window.setup(width=600, height=600)
 window.bgcolor('black')
 window.title('Snake Game')
 
+worm_list = []
+
+
+def create_worm(x, y):
+    worm = Turtle(shape='square')
+    worm.color('white')
+    worm.penup()
+    worm.setposition(x, y)
+    worm_list.append(worm)
+
+
+x_position = 0
+y_position = 0
+for _ in range(3):
+    create_worm(x_position, y_position)
+    x_position += -20
+
+
 window.exitonclick()
