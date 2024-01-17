561e6ac6f92e2c3398465d2c299cb764d745d8d8
rox-streuli <roxstreuli@gmail.com>
2024-01-17 21:53:29 +0000

    Cancel .speed().

--- snake.py
+++ snake.py
@@ -16,7 +16,6 @@ class Snake:
             new_segment = Turtle(shape='square')
             new_segment.color('white')
             new_segment.penup()
-            new_segment.speed('slowest')
             new_segment.setposition(start_pos)
             self.body.append(new_segment)
 
