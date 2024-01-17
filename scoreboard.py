8231bf3bb6dabe33f950d4675357267305864dda
rox-streuli <roxstreuli@gmail.com>
2024-01-17 22:40:01 +0000

    Add increase_scoreboard method. Refactor refresh_scoreboard.

--- scoreboard.py
+++ scoreboard.py
@@ -8,11 +8,14 @@ class Scoreboard(Turtle):
         self.goto(0, 280)
         self.color('lightgreen')
         self.score = 0
-        self.write(f"Score: {self.score}", align='center', font=('verdana', 12,
-                                                                 'normal'))
+        self.refresh_scoreboard()
 
     def refresh_scoreboard(self):
+        self.write(f"Score: {self.score}", align='center', font=('verdana',
+                                                                 12, 'normal'))
+
+    def increase_scoreboard(self):
+        """Increase scoreboard by 1 and refresh .write()"""
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align='center', font=('verdana', 12,
-                                                     'normal'))
+        self.refresh_scoreboard()
