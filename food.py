352185278769d8fa125ed41952f44b70c875c856
rox-streuli <roxstreuli@gmail.com>
2024-01-17 21:03:05 +0000

    Create food.py. Food class inherits from Turtle class.

---
+++ food.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+from random import randrange
+
+class Food(Turtle):
+    def __init__(self):
+        """Instantiate a Food object. Inherits from Turtle class."""
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color('cyan')
+        self.speed('fastest')
+        self.goto(randrange(-280, 280), randrange(-280, 280))
