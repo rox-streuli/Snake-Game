e286cf515edc1bb896c54e4d3dd31268c33098b5
rox-streuli <roxstreuli@gmail.com>
2024-01-17 18:31:54 +0000

    Refactored classes and methods to match online exercise. But I did not need it! I found the bug after doing all that.

--- snake.py
+++ snake.py
@@ -3,25 +3,46 @@ from turtle import Turtle
 SARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
-class CreateSegment:
-    def __init__(self, start):
-        """Instantiate a snake segment."""
-        self.segment = Turtle(shape='square')
-        self.segment.color('white')
-        self.segment.speed('fast')
-        self.segment.penup()
-        self.segment.setposition(start)
-
 class Snake:
-    """Intantiate a snake body."""
+    """Instantiate a snake body."""
     def __init__(self):
         self.body = []
+        self.create_snake()
+        self.head = self.body[0]
+
+    def create_snake(self):
+        """Create starting snake body."""
         for start_pos in SARTING_POSITIONS:
-            self.body.append(CreateSegment(start_pos))
+            new_segment = Turtle(shape='square')
+            new_segment.color('white')
+            new_segment.penup()
+            new_segment.setposition(start_pos)
+            self.body.append(new_segment)
+
 
     def move(self):
-        """Shift position of each segment in the snake."""
+        """Shift position of each segment in the snake body."""
         for section in range(len(self.body) -1, 0, -1):
-            new_pos = self.body[section -1].segment.position()
-            self.body[section].segment.setposition(new_pos)
-        self.body[0].segment.forward(MOVE_DISTANCE)
+            new_pos = self.body[section -1].position()
+            self.body[section].setposition(new_pos)
+        self.head.forward(MOVE_DISTANCE)
+
+    def head_up(self):
+        """Change head heading to 90 if not 270."""
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def head_right(self):
+        """Change head heading to 0 if not 180."""
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def head_left(self):
+        """Change head heading to 180 if not 0."""
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def head_down(self):
+        """Change head heading to 270 if not 90."""
+        if self.head.heading() != 90:
+            self.head.setheading(270)
