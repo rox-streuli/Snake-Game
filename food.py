e3e162954c939346f53fd270427166b2b294c2f5
rox-streuli <roxstreuli@gmail.com>
2024-01-21 19:42:14 +0000

    Refactor self.shapesize()

--- food.py
+++ food.py
@@ -7,7 +7,7 @@ class Food(Turtle):
         super().__init__()
         self.shape('circle')
         self.penup()
-        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.shapesize(stretch_wid=0.7, stretch_len=0.7)
         self.color('cyan')
         self.speed('fastest')
         self.refresh()
