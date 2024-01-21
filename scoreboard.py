aa15163603f36aebf58dd1f9cd1aae0ce5494d82
rox-streuli <roxstreuli@gmail.com>
2024-01-21 19:40:25 +0000

    Refactor increase_scoreboard() to check for winner score. refresh_scoreboard() to include self.level. game_over() to accept win parameter. Add Constants. Add level and current_speed attributes.

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
+
 ALIGMENT = 'center'
 FONT = ('verdana', 12, 'normal')
+SCORE_BOUNDARIES = [5, 10, 15, 25]
+LEVELS = {2: 0.3, 3: 0.2, 4: 0.1}
+WINNER = 35
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -10,18 +14,35 @@ class Scoreboard(Turtle):
         self.goto(0, 280)
         self.color('lightgreen')
         self.score = 0
+        self.level = 1
         self.refresh_scoreboard()
+        self.current_speed = 0.4
 
     def refresh_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGMENT, font=FONT)
+        """Update current level and score banner."""
+        self.write(f"Level: {self.level}\tScore: {self.score}",
+                   align=ALIGMENT, font=FONT)
 
     def increase_scoreboard(self):
         """Increase scoreboard by 1 and refresh .write()"""
         self.score += 1
-        self.clear()
-        self.refresh_scoreboard()
+        if self.score == WINNER:
+            self.game_over("YOU WON!")
+        else:
+            self.check_level()
+            self.clear()
+            self.refresh_scoreboard()
 
-    def game_over(self):
+    def check_level(self):
+        """Check if player reach boundary scores,
+         and increase one level."""
+        for score in SCORE_BOUNDARIES:
+            if self.score == score:
+                self.level += 1
+                self.current_speed = LEVELS[self.level]
+
+    def game_over(self, win='GAME OVER'):
+        """Write game over message on window."""
         self.goto(0, 0)
         self.clear()
-        self.write("**** GAME OVER ****", align=ALIGMENT, font=FONT)
+        self.write(f"**** {win} ****", align=ALIGMENT, font=FONT)
