0e53566fba34978dae3485c7eaffe7726575edf5
rox-streuli <roxstreuli@gmail.com>
2024-03-13 18:56:58 +0000

    Add from scoreboard import Guide. Add quit_game() function for quit game when 'q' key is pressed. Create Guide() object.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
-from scoreboard import Scoreboard
+from scoreboard import Scoreboard, Guide
 import time
+
+
+def quit_game():
+    game_is_on = False
+
 
 # create window
 window = Screen()
@@ -13,21 +18,27 @@ window.title('Snake Game')
 # Turn off .tracer()
 window.tracer(0)
 
-# Initiate snake
+# create snake
 my_snake = Snake()
 
-# Initiate food
+# create food
 next_food = Food()
 
-# Initiate scoreboard
+# create main scoreboard
 scoreboard_banner = Scoreboard()
+
+# create game guide
+guide = Guide()
 
 # Collect key-events
 window.listen()
+
 window.onkey(my_snake.head_down, key='Down')
 window.onkey(my_snake.head_up, key='Up')
 window.onkey(my_snake.head_right, key='Right')
 window.onkey(my_snake.head_left, key='Left')
+
+window.onkey(quit_game, key='q')
 
 game_is_on = True
 while game_is_on:
@@ -46,12 +57,10 @@ while game_is_on:
     y = my_snake.head.ycor()
     if x < -290 or x > 290 or y < -290 or y > 290:
         scoreboard_banner.game_over()
-        game_is_on = False
 
     # Detect collision with tail
     for segment in my_snake.body[4:]:
         if my_snake.head.distance(segment) < 10:
             scoreboard_banner.game_over()
-            game_is_on = False
 
 window.exitonclick()
